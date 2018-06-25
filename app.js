ac3ea441bdfaa080ae5b877decab2ba8c19879be
Peter Estes <peternigelestes@gmail.com>
2018-06-25 10:48:06 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config()
+const express = require("express")
 const app = express()
+const port = process.env.PORT || 5001
+const NodeHTTPError = require("node-http-error")
+const { pathOr, propOr, isEmpty, compose, not, join } = require("ramda")
+const reqFieldChecker = require("./lib/required-field-check")
+const {
+	// addBoard,
+	// getBoard,
+	// listBoards,
+	// updateBoard,
+	// deleteBoard
+} = require("./dal")
 
-app.get('/', function(req, res, next) {
-  res.send('Welcome to the Art API. Manage all the paintings.')
+app.use(bodyParser.json())
+
+app.get("/", function(req, res, next) {
+	res.send("Welcome to the Art API. Manage all the paintings.")
 })
+
+app.listen(port, () =>
+	console.log("Good artists borrow... Great artists steal :)", port)
+)
